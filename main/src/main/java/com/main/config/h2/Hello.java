d6ea5137eff49d93d59374a50b9317d0b6098ff6
bantangz <${2435054660@qq.com}>
2023-08-20 18:58:13 +0800

    success

--- main/src/main/java/com/main/config/h2/Hello.java
+++ main/src/main/java/com/main/config/h2/Hello.java
@@ -5,5 +5,6 @@ public class Hello {
         System.out.println("哈哈2");
         System.out.println("哈哈哈3");
         System.out.println("你好hotfix");
+        System.out.println("success");
     }
 }
